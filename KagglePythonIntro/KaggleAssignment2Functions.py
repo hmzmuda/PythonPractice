f6e4b6634675c4ca33975d3a9afeb2e20669d2ca
Hannah Zmuda <hannah.zmuda@wustl.edu>
2020-07-20 12:51:26 -0500

    Finished Functions Section in Kaggle

--- KagglePythonIntro/KaggleAssignment2Functions.py
+++ KagglePythonIntro/KaggleAssignment2Functions.py
@@ -19,3 +19,35 @@ print(
 )
 #help(leastDifference)
 #function without retun prints None None None
+#Default arguments, so bult in functions have default arguments, such as sep in the print function
+print(1, 2, 3, sep = ' < ')
+def greet(who = "Colin"):
+    print("Hello, ", who)
+greet()
+greet(who = "Kaggle")
+greet("world")
+#Functions Applied to Functions
+def multByFive(x):
+    return 5 * x
+def call(fn, arg):
+    """Call fn on arg"""
+    return fn(arg)
+def squaredCall(fn, arg):
+    """Call fn on the result of calling fn on arg"""
+    return fn(fn(arg))
+print(
+    call(multByFive, 1),
+    squaredCall(multByFive, 1),
+    sep = '\n',
+)
+def mod5(x):
+    """Return ther remainder of x after by 5"""
+    return x % 5
+print(
+    'Which number is biggest?',
+    max(100, 51, 14),
+    'Which number is the biggest modulo 5?',
+    max(100, 51, 14, key = mod5), #But if we pass in a function using the optional key argument, it returns the argument x that maximizes key(x) (aka the 'argmax')
+    sep = '\n',
+)
+help(round)
