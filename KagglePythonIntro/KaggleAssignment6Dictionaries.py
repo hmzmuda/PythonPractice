a9768a6c776c79ff780f7086d4fcf40bf0ccc24b
Hannah Zmuda <hannah.zmuda@wustl.edu>
2020-07-22 13:35:18 -0500

    Started Wokring on Assignment 6 on Kaggle python Tutorial

---
+++ KagglePythonIntro/KaggleAssignment6Dictionaries.py
@@ -0,0 +1,8 @@
+#This Kaggle lesson looks into Strings and Dictionary data ypes in python
+print('Pluto\'s a planet')
+planet = 'Pluto'
+print(planet[0])
+print(planet[-3:])
+print([char + '!' for char in planet])
+
+#String methods
